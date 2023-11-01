e7e30772a6fe8bb012980377b30252bb9983addc
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-11-01 15:25:39 +0000

    added exercises as object array

--- js/main.js
+++ js/main.js
@@ -4,9 +4,21 @@ const exercise = document.getElementById("exercise");
 const duration = document.getElementById("duration");
 const reps = document.getElementById("reps");
 const interval = document.getElementById("interval");
+let parsedSavedExercises = []
+
+const savedExercises = localStorage.getItem('completeExercises')
+if (savedExercises) {
+  parsedSavedExercises = JSON.parse(savedExercises)
+  console.log(parsedSavedExercises)
+} else {
+  parsedSavedExercises = []
+  console.log("No saved exercises")
+}
+
 
 showExercises()
-
+// const completeExercises = parsedSavedExercises
+// console.log(completeExercises)
 function showHeadings () {
   const li = document.createElement("li")
   const exerciseSpan = document.createElement("span")
@@ -34,6 +46,7 @@ function showHeadings () {
   li.appendChild(deleteSpan)
 
   exerciseList.appendChild(li)
+
   
 }
 
@@ -61,7 +74,21 @@ function addItemToList() {
   intervalSpan.setAttribute("class", "interval-format")
   deleteSpan.setAttribute("id", "deleteSpanId")
   li.setAttribute("id", "listItem")
-
+  li.setAttribute("class", "listItemClass")
+  
+  let uniqueId = new Data().getTime()
+  
+  const exerciseData = {
+    id: uniqueId,
+    exercise: exercise.value,
+    duration: duration.value,
+    reps: reps.value,
+    interval: interval.value
+  }
+  
+  parsedSavedExercises.push(exerciseData)
+  console.log(parsedSavedExercises)
+  
   exerciseSpan.innerHTML = exercise.value
   exercise.value = ""
   durationSpan.innerHTML = duration.value
@@ -83,16 +110,32 @@ function addItemToList() {
   saveData()
 }
 
+// function exerciseArray() {
+//   const savedExercises = localStorage.getItem('completeExercises')
+//   if (savedExercises) {
+//     const parsedSavedExercises = JSON.parse(savedExercises)
+//     console.log(parsedSavedExercises)
+//   } else {
+//     const parsedSavedExercises = []
+//     console.log("No saved exercises")
+//   }
+// }
+
+
+
+
 function saveData() {
   localStorage.setItem("data", exerciseList.innerHTML)
+  localStorage.setItem("completeExercises", JSON.stringify(parsedSavedExercises) )
+  
+  
 }
 
 function showExercises() {
   exerciseList.innerHTML = localStorage.getItem("data")
   if (!exerciseList.hasChildNodes()) {
     showHeadings()
-  }
-  
+  }  
 }
 
 // delete exercise from list
@@ -130,6 +173,7 @@ function handleStartClick() {
 function handleClearClick() {
   exerciseList.innerHTML = ""
   showHeadings()
+  saveData()
 }
 
 
@@ -146,30 +190,44 @@ function handleClearClick() {
 
 
 
-console.log(interval.value)
-let timeleft = interval.value;
+// for (i = 0; i < intervalArray.length; i++) {
+//   let timeleft = intervalArray[i]
+//   console.log(timeleft)
+// const downloadTimer = setInterval(function(){
+//   if(timeleft <= 0){
+//     document.getElementById("countdown").innerHTML = ""
+//   }
+//   document.getElementById("countdown").innerHTML = timeleft;
+//   // while (timeleft >= 0) {
+//   //   timeleft -= 1;
+//   // }
+//   timeleft -= 1
+// }, 1000);
+// }
 
-const downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-  }
-  document.getElementById("progressBar").value = 10 - timeleft;
-  timeleft -= 1;
-}, 1000);
   
-const ul = document.getElementById("exercise-list");
-const items = ul.getElementsByTagName("li");
-// const li2 = document.querySelector("listItem")
+
+
+
+// const ul = document.getElementById("exercise-list");
+// const items = ul.getElementsByTagName("li");
+
+// // const li2 = document.querySelector("listItem")
+// // const spans = li2.getElementsByTagName("span")
+// intervalArray = []
+// for (let i = 1; i < items.length; i++) {
+// //  let intervals = document.querySelector("interval-format").innerText
+// //  console.log(intervals) 
+
+// const li2 = document.getElementById("listItem")
 // const spans = li2.getElementsByTagName("span")
-for (let i = 1; i < items.length; i++) {
-//  let intervals = document.querySelector("interval-format").innerText
-//  console.log(intervals) 
-const li2 = document.getElementById("listItem")
-const spans = li2.getElementsByTagName("span")
- console.log(items[i])
- console.log(spans[3])
- console.log(spans)
-}
+//  console.log(items[i])
+//  console.log(spans[3].innerText)
+//  console.log(spans)
+ 
+//  intervalArray.push(li2.querySelector(".interval-format").innerText)
+//  console.log(intervalArray)
+//}
 
 
 
